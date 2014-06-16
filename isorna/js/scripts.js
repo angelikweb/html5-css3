c14ce6e3de9fe65f462d5787460e9cc0d5f1e849
fictiziaformacion <alvaro@Z00.local>
2014-06-16 13:54:54 +0200

    Lunes 16 de Junio

--- isorna/js/scripts.js
+++ isorna/js/scripts.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+function validacion(){
+	if (document.getElementById('texto').value == '') {
+		alert('Por favor rellena el texto');
+		return false;
+	} else {
+		document.getElementById('formulario').style.marginTop = Number(document.getElementById('texto').value) + 'px';
+		return false;
+	}
+}
+
+
+function cambiaEnlaces(color){
+	var enlaces = document.getElementsByTagName('A');
+	var indice = 0;
+	
+	for (indice = 0; indice < enlaces.length; indice++) {
+		enlaces[indice].style.backgroundColor = color;
+	}
+}
